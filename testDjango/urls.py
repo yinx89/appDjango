107c070ecf20e595a5cdb71434f3fb201b9e5c0a
Ginés Molina <gines.moli@gmail.com>
2019-12-15 01:54:54 +0000

    Last edition Saturday

--- testDjango/urls.py
+++ testDjango/urls.py
@@ -13,10 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    url('testapp/',include('testapp.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^testapp/',include('testapp.urls', namespace='testapp')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
